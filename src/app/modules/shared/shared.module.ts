ca1b4fa9ab3efe75e2f73e8bccf8ca3051f914f1
IrynaPanchuk97 <rusa1234527@gmail.com>
2019-06-13 20:20:18 +0300

    carousel

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MainPageComponent } from './components/main-page/main-page.component';
-import { SportComponent } from './components/sport/sport.component';
-import { YoutubeComponent } from './components/youtube/youtube.component';
-
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { MainPageComponent } from "./components/main-page/main-page.component";
+import { SportComponent } from "./components/sport/sport.component";
+import { YoutubeComponent } from "./components/youtube/youtube.component";
+import { MDBBootstrapModule } from "angular-bootstrap-md";
+import { BrowserModule } from "@angular/platform-browser";
 @NgModule({
   declarations: [MainPageComponent, SportComponent, YoutubeComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, BrowserModule, MDBBootstrapModule.forRoot()],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class SharedModule { }
+export class SharedModule {}
