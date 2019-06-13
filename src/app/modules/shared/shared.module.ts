86ebd903520e941972ed6ebbe93f30c84076d803
IrynaPanchuk97 <rusa1234527@gmail.com>
2019-06-13 19:44:33 +0300

    add

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './components/main-page/main-page.component';
+import { SportComponent } from './components/sport/sport.component';
+import { YoutubeComponent } from './components/youtube/youtube.component';
 
 @NgModule({
-  declarations: [MainPageComponent],
+  declarations: [MainPageComponent, SportComponent, YoutubeComponent],
   imports: [
     CommonModule
   ]
