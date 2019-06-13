586fb95485f382d570fce17bf1f5f799691b0181
IrynaPanchuk97 <rusa1234527@gmail.com>
2019-06-13 19:41:24 +0300

    nav tabs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { MainPageComponent } from "./modules/shared/components/main-page/main-page.component";
+import { SportComponent } from "./modules/shared/components/sport/sport.component";
+import { YoutubeComponent } from "./modules/shared/components/youtube/youtube.component";
 
 const routes: Routes = [
   {
     path: "news",
     component: MainPageComponent
+  },
+  {
+    path: "sport",
+    component: SportComponent
+  },
+  {
+    path: "YouTube",
+    component: YoutubeComponent
   },
   {
     path: "**",
