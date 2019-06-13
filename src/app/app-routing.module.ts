10569b292998507a887841d5dca34d2f63d8d690
IrynaPanchuk97 <rusa1234527@gmail.com>
2019-06-13 19:06:40 +0300

    first template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { MainPageComponent } from "./modules/shared/components/main-page/main-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "news",
+    component: MainPageComponent
+  },
+  {
+    path: "**",
+    redirectTo: "/news"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
